341ee82e85869ca8be70c851b98abbf9bfeecc67
Martin <martin_lenart@icloud.com>
2025-08-28 12:46:32 +0200

    microsoft template

--- DbContext/Migrations/PostgresDbContext/20250828104603_miInitial.Designer.cs
+++ DbContext/Migrations/PostgresDbContext/20250828104603_miInitial.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DbContext.Migrations.PostgresDbContext
 {
     [DbContext(typeof(MainDbContext.PostgresDbContext))]
-    [Migration("20250828104004_miInitial")]
+    [Migration("20250828104603_miInitial")]
     partial class miInitial
     {
         /// <inheritdoc />
